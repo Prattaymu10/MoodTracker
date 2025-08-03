d0429b78a95eb9cc3294ae4bbedf5df1d613e0e5
Prattaymu10 <prattaymu10@gmail.com>
2025-08-03 16:39:29 +0600

    updated server.js for render

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/moods", require("./routes/mood"));
+
+// PORT fallback for local use
+const PORT = process.env.PORT || 5000;
+
+// DB Connection and Server Start
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error("MongoDB connection error:", err));
